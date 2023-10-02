6fddb5254aaba8ef802d1b1d826dd0d86483dc24
Vaidas V <valikonis.vaidas@gmail.com>
2023-10-02 13:52:24 +0300

    Updated printer method

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -1,14 +1,16 @@
 package com.example.jenkinstest;
 
+import com.example.jenkinstest.service.TestService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsTestApplication {
+    private final static TestService testService = new TestService();
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
-        System.out.println("Hello world");
+        testService.printer();
         System.exit(0);
     }
 
---
+++ src/main/java/com/example/jenkinstest/service/TestService.java
@@ -0,0 +1,11 @@
+package com.example.jenkinstest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public void printer(){
+        System.out.println("Hello world to jenkins");
+    }
+}
